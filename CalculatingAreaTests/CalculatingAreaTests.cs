320786a15c8354e88a553ef4cae11a5d3179027c
yakimovaa1998@mail.ru <yakimovaa1998@mail.ru>
2020-07-08 04:42:49 +0300

    Добавьте файлы проекта.

---
+++ CalculatingAreaTests/CalculatingAreaTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatingArea;
+
+namespace CalculatingAreaTests
+{
+    [TestClass]
+    public class CalculatingAreaTests
+    {
+        [TestMethod]
+        public void CalculateTriangleAreaTest()
+        {
+            //Arrange
+            double a = 3.0;
+            double b = 4.0;
+            double c = 5.0;
+            double s = 6;
+            Triangle triangle = new Triangle(a, b, c);
+            //Act
+            double area = triangle.Area();
+            //Assert
+            Assert.AreEqual(s,area);
+        }
+        [TestMethod]
+        public void CheckIfTriangleIsRight()
+        {
+            //Arrange
+            double a = 3.0;
+            double b = 4.0;
+            double c = 5.0;
+            Triangle triangle = new Triangle(a, b, c);
+            //Act
+            bool testResult = triangle.CheckTriangle();
+            //Assert
+            Assert.IsTrue(testResult);
+        }
+        [TestMethod]
+        public void CalculateCircleAreaTest()
+        {
+            //Arrange
+            double radius = 5.0;
+            double expectedArea = Math.PI*5*5;
+            Circle circle = new Circle(radius);
+            //Act
+            double area = circle.Area();
+            //Assert
+            Assert.AreEqual(expectedArea, area);
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingArea
+{
+    public class Triangle : Figure
+    {
+        private double sideA, sideB, sideC;
+
+        public double SideA
+        {
+            get { return sideA; }
+            set { sideA = value < 0 ? -value : value; }
+        }
+
+        public double SideB
+        {
+            get { return sideB; }
+            set { sideB = value < 0 ? -value : value; }
+        }
+
+        public double SideC
+        {
+            get { return sideC; }
+            set { sideC = value < 0 ? -value : value; }
+        }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+            {
+                SideA = sideA;
+                SideB = sideB;
+                SideC = sideC;
+            }
+            else
+            {
+                Console.WriteLine("It is not a triangle");
+                return;
+
+            }
+        }
+
+        public override double Area()
+        {
+            double Perimeter = (SideA + SideB + SideC) / 2;
+            return (Math.Sqrt(Perimeter * (Perimeter - SideA) * (Perimeter - SideB) * (Perimeter - SideC)));
+
+        }
+
+        public bool CheckTriangle()
+        {
+            if (SideA * SideA == (SideB * SideB + SideC * SideC) || SideB * SideB == (SideC * SideC + SideA * SideA) ||
+                SideC * SideC == (SideB * SideB + SideA * SideA))
+                return (true);
+
+            return (false);
+
+        }
+
+    }
+}
